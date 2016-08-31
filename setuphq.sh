9dfdcd845394659ba31b5fdd72867bb37a241b9c
Sean Dowd <dowds@avalonconsult.com>
2016-08-31 14:50:49 -0500

    changes for esbackup repository, more curator examples, more plugin installers

---
+++ setuphq.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# Install HQ  monitoring on all nodes in example cluster.
+# This assumes that you have started the es-demo-cluster project via
+# docker-compose.  You can get teh es-demo-cluster here:
+# git@github.com:mcascallares/es-demo-cluster.git
+
+# You need to run this script in that repository.
+
+docker exec -it elasticsearch bin/plugin install royrusso/elasticsearch-HQ
+for i in $(seq 1 3); do
+  host=$(printf "master%.2d\n" $i)
+  echo $host
+  docker exec -it ${host} bin/plugin install royrusso/elasticsearch-HQ
+  echo ${host} done
+done
+
+for i in $(seq 1 5); do
+  host=$(printf "data%.2d\n" $i)
+  echo ${host}
+  docker exec -it ${host} bin/plugin install royrusso/elasticsearch-HQ
+  echo ${host} done
+done
+
+docker-compose restart
+
